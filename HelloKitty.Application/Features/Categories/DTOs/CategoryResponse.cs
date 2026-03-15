b492137c02c20c355734a24bf98269b0991ea08c
Nguyen Quang Diep <nguyenquangdiepzed1@gmail.com>
2026-03-15 23:36:23 +0700

    feat:(category): implement category application module

---
+++ HelloKitty.Application/Features/Categories/DTOs/CategoryResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloKitty.Application.Features.Categories.DTOs
+{
+    public class CategoryResponse
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public string Slug { get; set; }
+        public bool IsActive { get; set; }
+        public Guid? ParentId { get; set; }
+        public List<CategoryResponse> children { get; set; }
+    }
+}
---
+++ HelloKitty.Application/Features/Categories/DTOs/UpdateCategoryRequest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloKitty.Application.Features.Categories.DTOs
+{
+    public class UpdateCategoryRequest
+    {
+        public string CategoryName { get; set; }
+        public string Slug { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
