9f2ef939af39b185d7a0af5cebae51c71927864e
Nguyen Quang Diep <nguyenquangdiepzed1@gmail.com>
2026-03-17 02:13:54 +0700

    feat: add function interface on Domain Orders

---
+++ HelloKitty.Domain/Orders/Interfaces/IOrderRepository.cs
@@ -0,0 +1,21 @@
+﻿using HelloKitty.Domain.Common;
+using HelloKitty.Domain.Common.Interfaces;
+using HelloKitty.Domain.Orders.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloKitty.Domain.Orders.Interfaces
+{
+    public interface IOrderRepository : IWriteRepository<Order>
+    {
+        Task AddAsync(Order order, CancellationToken ct = default);
+        Task<Order?> GetByIdWithDetailsAsync(Guid id, CancellationToken ct = default);
+        Task<PagedResult<Order>> GetByUserIdPagedAsync(
+            Guid userId, int page, int pageSize, CancellationToken ct = default);
+        Task<PagedResult<Order>> GetAllPagedAsync(
+            int page, int pageSize, CancellationToken ct = default);
+    }
+}
