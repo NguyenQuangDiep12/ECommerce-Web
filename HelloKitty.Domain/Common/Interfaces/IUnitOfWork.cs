1c0c14971176aac1cfb33b1d7cdf05763a1c09eb
Nguyen Quang Diep <nguyenquangdiepzed1@gmail.com>
2026-03-17 02:10:58 +0700

    feat: add interfaces into unit of work

--- HelloKitty.Domain/Common/Interfaces/IUnitOfWork.cs
+++ HelloKitty.Domain/Common/Interfaces/IUnitOfWork.cs
@@ -1,5 +1,9 @@
-﻿using HelloKitty.Domain.Inventory.Interfaces;
+﻿using HelloKitty.Domain.Carts.Interfaces;
+using HelloKitty.Domain.Catalog.Interfaces;
+using HelloKitty.Domain.Inventory.Interfaces;
 using HelloKitty.Domain.Logging.Interfaces;
+using HelloKitty.Domain.Orders.Interfaces;
+using HelloKitty.Domain.Promotions.Interfaces;
 using HelloKitty.Domain.Users.Interfaces;
 using System;
 using System.Collections.Generic;
@@ -11,17 +15,24 @@ namespace HelloKitty.Domain.Common.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
-
         IUserRepository Users { get; }
         IUserCredentialRepository UserCredentials { get; }
         IRefreshTokenRepository RefreshTokens { get; }
 
+        ICategoryRepository Categories { get; }
+        IProductRepository Products { get; }
+        IReviewRepository Reviews { get; }
+
+        ICartRepository Carts { get; }
+
+        IOrderRepository Orders { get; }
+
+        IVoucherRepository Vouchers { get; }
 
         IAuditLogRepository AuditLogs { get; }
         ISystemLogRepository SystemLogs { get; }
         IInventoryLogRepository InventoriesLogs { get; }
 
         Task<int> SaveChangesAsync(CancellationToken ct = default);
-
     }
 }
