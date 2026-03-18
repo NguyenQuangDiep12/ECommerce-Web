9ca0fcf422911722b69b7360f7a67ecdf6b9bafc
Nguyen Quang Diep <nguyenquangdiepzed1@gmail.com>
2026-03-18 19:05:26 +0700

    feat: Add the Order feature to the project.

--- HelloKitty.Application/Features/Orders/DTOs/CreateOrderRequest.cs
+++ HelloKitty.Application/Features/Orders/DTOs/CreateOrderRequest.cs
@@ -12,6 +12,7 @@ namespace HelloKitty.Application.Features.Orders.DTOs
         public string ReceiverName { get; set; } = string.Empty;
         public string Phone { get; set; } = string.Empty;
         public string District { get; set; } = string.Empty;
+        public string Province { get; set; } = string.Empty;
         public string Ward { get; set; } = string.Empty;
         public string Street { get; set; } = string.Empty;
         public string? VoucherCode { get; set; }
