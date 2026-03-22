f5eb7444dcb0d6a57010a4570770b3119d9343c3
Nguyen Quang Diep <nguyenquangdiepzed1@gmail.com>
2026-03-22 12:13:41 +0700

    feat: add new feature interfaces.

---
+++ HelloKitty.Domain/Users/Interfaces/IRoleRepository.cs
@@ -0,0 +1,16 @@
+﻿using HelloKitty.Domain.Users.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloKitty.Domain.Users.Interfaces
+{
+    public interface IRoleRepository
+    {
+        Task<IEnumerable<Role>> GetAllAsync(CancellationToken ct = default);
+        Task<Role?> GetByIdAsync(Guid roleId, CancellationToken ct = default);
+        Task<Role?> GetByNameAsync(string roleName, CancellationToken ct = default);
+    }
+}
