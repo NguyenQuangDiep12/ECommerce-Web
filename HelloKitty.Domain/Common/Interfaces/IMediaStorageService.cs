fae647641f760137b87c741fc608af49a745a2a8
Nguyen Quang Diep <nguyenquangdiepzed1@gmail.com>
2026-03-25 21:56:11 +0700

    refactor: Revise the code structure.

---
+++ HelloKitty.Domain/Common/Interfaces/IMediaStorageService.cs
@@ -0,0 +1,8 @@
+﻿namespace HelloKitty.Domain.Common.Interfaces
+{
+    public interface IMediaStorageService
+    {
+        Task<string> UploadAsync(Stream fileStream, string fileName, string contentType, string folder, CancellationToken ct = default);
+        Task DeleteAsync(string publicId, CancellationToken ct = default);
+    }
+}
